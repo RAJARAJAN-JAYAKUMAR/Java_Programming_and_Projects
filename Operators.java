c6fe5c377de64a8025d899be873c5d8c71900c8e
RAJARAJAN-JAYAKUMAR <jrajanraja1974@gmail.com>
2023-09-05 21:38:02 +0530

    'added'

--- Operators.java
+++ Operators.java
@@ -12,5 +12,23 @@ public class Operators {
         System.out.println(multiply);
         System.out.println(division); //answer is 0. How?
         System.out.println(remainder); //answer is &. How?
+
+        int n = 7;
+        // num3 = num3 +1; 
+        n += 1; //shorthand writing
+        ++n; //post-increment
+        System.out.println(n);
+        n++; //post-increment
+        System.out.println(n); // n is 10
+        // so how the pre increment and post increment differs, inplace of fetch the value 
+
+        int result = ++n;
+        System.out.println(result); // result = 11
+        //first increase the value and fetch the vlaue 'n'. and assign to the variable
+
+        int result1 = n++;
+        System.out.println(result1);
+        //fetching the value first and increase next. but the fetching value only assigned to the variable
+        
     }
 }
