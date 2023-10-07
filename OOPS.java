d3d4a954c1073a6f5bad1cdf246f8768f84cdf09
RAJARAJAN-JAYAKUMAR <jrajanraja1974@gmail.com>
2023-10-07 13:11:19 +0530

    'added'

---
+++ OOPS.java
@@ -0,0 +1,37 @@
+// //* object oriented programming language
+// object - Behaviour but a blueprint
+// */
+// class calculator{
+//     public int add() //the method returns the value in int
+//     {
+//         System.out.println('return value')
+//     }
+// }
+
+
+
+// public class OOPS {
+//     public static void main(String[] args) {
+
+//     }
+// }
+class calculator{
+    public int add(int n1, int n2){
+        int r = n1 + n2;
+        return r;
+    }
+    
+}
+
+
+public class OOPS{
+    public static void main(String[] args){
+        int n1 = 4;
+        int n2 = 5;
+        calculator calc = new calculator();
+        int result = calc.add(n1,n2);
+        System.out.println(result);
+    }
+}
+
+
