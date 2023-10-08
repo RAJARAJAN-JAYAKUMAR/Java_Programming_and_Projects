8b560f7d22383f6ad9d4ad3c8dfe2799f92e042f
RAJARAJAN-JAYAKUMAR <jrajanraja1974@gmail.com>
2023-10-08 21:32:27 +0530

    'added'

---
+++ Algorithms/Mergesort.java
@@ -0,0 +1,5 @@
+package Algorithms;
+
+public class Mergesort {
+    public static void main(String[] args) {}
+}
--- z.java
+++ z.java
@@ -1,6 +1,6 @@
 public class z {
     public static void main(String[] args) {
-        for( int i = 0; i <5; i++){
+        for( int i = 1; i<5; i++){
             System.out.print(i);
         }
     }
